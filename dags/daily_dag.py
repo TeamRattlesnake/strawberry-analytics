f743e89170a53173cb053f595984db943e104adb
nokrolikno <vasya.ermakov.2001@mail.ru>
2023-05-10 03:11:23 +0300

    for send

---
+++ dags/daily_dag.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from datetime import timedelta
+
+from airflow.models import Variable
+from airflow.providers.docker.operators.docker import DockerOperator
+from datetime import datetime
+from docker.types import Mount
+
+
+DATA_DIR = Variable.get('DATA_DIR')
+
+default_args = {
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+
+with DAG(
+    'daily',
+    default_args=default_args,
+    schedule_interval='@daily',
+    start_date=datetime(2023, 5, 9)
+) as dag:
+    daily = DockerOperator(
+        image='airflow-daily',
+        command='--output-dir /data/raw/{{ ds }}',
+        network_mode='bridge',
+        task_id='docker-airflow-daily',
+        do_xcom_push=False,
+        auto_remove=True,
+        mounts=[Mount(source=DATA_DIR, target='/data', type='bind')]
+    )
+
+    daily
