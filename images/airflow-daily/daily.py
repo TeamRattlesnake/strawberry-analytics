7d2b067e5fe8a2a144dc3aca180d89f10630ee3d
nokrolikno <vasya.ermakov.2001@mail.ru>
2023-05-10 14:10:27 +0300

    daily

--- images/airflow-daily/daily.py
+++ images/airflow-daily/daily.py
@@ -15,8 +15,11 @@ def generate_daily_analytics(output_dir="data"):
     db = Database(user, password, database, port, host)
     a = db.get_all()
     methods = {}
+    yesterday = datetime.date.today() - datetime.timedelta(1)
+    unix_time = yesterday.strftime("%s")
     for item in a:
-        methods[item.method] = methods.get(item.method, 0) + 1
+        if int(item.date) >= int(unix_time):
+            methods[item.method] = methods.get(item.method, 0) + 1
     df = pd.DataFrame(methods)
     df.to_csv(f"{output_dir}/daily-{datetime.today().strftime('%d-%m-%Y')}.csv")
 
