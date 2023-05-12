7a7f741a86bdd1245d12d9fd1c768f766fcf63d2
nokrolikno <vasya.ermakov.2001@mail.ru>
2023-05-12 13:05:21 +0300

    changed graphs

--- images/airflow-monthly-graph/monthly-graph.py
+++ images/airflow-monthly-graph/monthly-graph.py
@@ -1,21 +1,22 @@
 import os
 import matplotlib.pyplot as plt
 
-from datetime import datetime
+from datetime import datetime, date, timedelta
 
 
 def generate_daily_graph(input_dir="data/tmp", output_dir="data/results/monthly"):
     os.makedirs(output_dir, exist_ok=True)
     X = []
     Y = []
-    with open (f"{input_dir}/monthly-{datetime.today().strftime('%d-%m-%Y')}", 'r') as f:
+    with open(f"{input_dir}/monthly-{datetime.today().strftime('%d-%m-%Y')}", 'r') as f:
         for line in f:
             x, y = line.strip().split(';')
-            X.append(int(x))
+            cur_date = date.today() - timedelta(days=(30-int(x)))
+            X.append(cur_date.strftime("%d.%m"))
             Y.append(int(y))
 
     fig, ax = plt.subplots()
-    ax.plot(X, Y)
+    ax.bar(X, Y, color='maroon', width=0.4)
 
     fig.savefig(f"{output_dir}/{datetime.today().strftime('%d-%m-%Y')}.png")
 
